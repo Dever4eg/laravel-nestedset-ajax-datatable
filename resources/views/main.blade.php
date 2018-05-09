e75ceb8e69a65b657d35492cf96f4ff852e40ad4
dever <dever4eg@gmail.com>
2018-05-10 01:01:18 +0300

    Display employees position and fullname

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,9 +6,33 @@
 
 @section('scripts')
     <script>
-        $('#tree').tree({
-            textField: 'fullname',
-            dataSource: '/api/employees'
+
+        $(document).ready(function(){
+            $.ajax({
+                url: "/api/employees",
+                method:"get",
+                dataType: "json",
+                success: function (data)
+                {
+                    data = (function proc(data) {
+                        data.forEach(function(item, i, data){
+
+                            // Displayed text
+                            data[i].text = data[i].fullname + ", " + data[i].position;
+
+                            if(data[i].children)
+                                data[i].children =  proc(data[i].children);
+                        } );
+                        return data;
+                    }(data));
+
+                    $('#tree').tree({
+                        textField: 'text',
+                        dataSource: data
+                    });
+                }
+            });
+
         });
     </script>
 @endsection

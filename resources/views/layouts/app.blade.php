8d0715c7bac110d48fb9072ae8cacd2dd8eea8c9
dever <dever4eg@gmail.com>
2018-05-22 20:19:16 +0300

    Make navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="api-token" content="{{ Auth::guest() ? '' : Auth::user()->api_token }}">
 
@@ -14,8 +15,9 @@
 </head>
 <body>
 
+    @include('parts.nav')
+
     <div class="container">
-        <h1>Abz.agency тестовое задание</h1>
         @yield('content')
     </div>
 
